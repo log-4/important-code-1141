7d51a9576c5231352e9199cdf72bccb15a16076a
logga <117048930+log-4@users.noreply.github.com>
2022-11-16 17:16:11 +0100

    Add files via upload
    important code belongs into Github arctic code vault

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ich will VVS und kein'n Strass\n" +
+                "Ich bin im Studio, hab' Kunden verpasst\n" +
+                "Kripos stell'n Fragen, ich sage kei'm was\n" +
+                "Gestern war knapp, sie hatten mich fast\n" +
+                "Mit der Gang, immer auf dem Hof, wie Gas\n" +
+                "Handschell'n eng, weil der Bulle, hat Hass\n" +
+                "Ich hab' ein'n blauen Pass\n" +
+                "Kunde wartet vor Starbucks, du weißt, auf mich ist Verlass\n" +
+                "Hier ist Kunde nicht König, du bist nur ein Gast\n" +
+                "Stopfe die Tüten voll, bis nix mehr passt\n" +
+                "Ölring, weil ich bau' Zkittlez, mit Wachs (Hajar)\n" +
+                "Und ich trink' Lean, Medizin, mach' mich blass (Gang)\n" +
+                "Geb' ihm ein'n Ball, ein'n Pass (Ein'n Ball, ey), ey\n" +
+                "Fünfzehn Gramm Buds, dick, wie ein Ast\n" +
+                "Hase so frisch, noch fast nass, alles klebt\n" +
+                "Das Paket, was ich nur mit Handschuh'n anfass' (Ey, ey, ey)");
+    }
+}
